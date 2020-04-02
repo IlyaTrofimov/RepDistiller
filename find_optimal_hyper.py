2b03448ef488da4f2c4d7bccad6d5de87b41d5c3
IlyaTrofimov <ilya.trofimov@skoltech.ru>
2020-04-02 18:48:27 +0300

    Update hyperopt script.

--- find_optimal_hyper.py
+++ find_optimal_hyper.py
@@ -5,6 +5,11 @@ import itertools, functools
 distill_type = sys.argv[1]
 gpu = int(sys.argv[2])
 
+if len(sys.argv) >= 4:
+    is_dry_run = True
+else:
+    is_dry_run = False
+
 def hyper_gen():
 
     configs = [None] * (len(param_names) + 1)
@@ -20,10 +25,10 @@ if distill_type == 'crd':
     param_values = [[0.02, 0.05, 0.1, 0.2], [0.5, 1.0, 2.0, 4.0]]
 elif distill_type == 'pkt':
     param_names = ['beta']
-    param_values = [[0.75e3, 1.5e3, 3e3, 6e3, 12e3]]
+    param_values = [[0.75e4, 1.5e4, 3e4, 6e4, 12e4]]
 elif distill_type == 'similarity':
     param_names = ['beta']
-    param_values = [[0.75e4, 1.5e4, 3e4, 6e4, 12e4]]
+    param_values = [[0.75e3, 1.5e3, 3e3, 6e3, 12e3]]
 elif distill_type == 'vid':
     param_names = ['beta']
     param_values = [[0.25, 0.5, 1, 2, 4]]
@@ -47,5 +52,6 @@ if __name__ == '__main__':
         print(s)
         cmd = 'python train_student.py --gpu %d --part 9 --distill %s --model_s MobileNetV2Trofim -r 1 -a 0 --epochs 100 --nce_k 4096 %s --prefix %s_tune/%s_per11_%d_ ' % (gpu, distill_type, s, distill_type, distill_type, cnt % configs_num)
         print(cmd)
-        os.system(cmd)
+        if not is_dry_run:
+            os.system(cmd)
         cnt += 1
