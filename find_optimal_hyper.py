bb79f40bc80233b03d5adcf0f57082b0d2545225
Ilya Trofimov <i.trofimov@skoltech.ru>
2020-04-03 12:12:26 +0300

    Add done status.

--- find_optimal_hyper.py
+++ find_optimal_hyper.py
@@ -20,12 +20,23 @@ def hyper_gen():
 
     return itertools.product(*configs)
 
+def done_gen():
+
+    configs = [None] * (len(param_names) + 1)
+    configs[0] = ['--arc %d' % arc for arc in range(20)]
+
+    for i in range(len(param_names)):
+        configs[i+1] = [param_done[i][j] for j in range(len(param_values[i]))]
+
+    return itertools.product(*configs)
+
 if distill_type == 'crd':
     param_names = ['nce_t', 'beta']
     param_values = [[0.02, 0.05, 0.1, 0.2], [0.5, 1.0, 2.0, 4.0]]
 elif distill_type == 'pkt':
     param_names = ['beta']
-    param_values = [[0.75e4, 1.5e4, 3e4, 6e4, 12e4]]
+    param_values = [[0.75e4, 1.5e4, 3e4, 6e4, 12e4, 24e4, 48e4, 96e4]]
+    param_done = [[1,         1,     1,   1,    1,   0,    0,    0  ]]
 elif distill_type == 'similarity':
     param_names = ['beta']
     param_values = [[0.75e3, 1.5e3, 3e3, 6e3, 12e3]]
@@ -39,6 +50,9 @@ elif distill_type == 'nst':
     param_names = ['beta']
     param_values = [[12.5, 25, 50, 100, 200]]
 
+def mult(arr):
+    return functools.reduce(lambda x, y: x*y, arr)
+
 if __name__ == '__main__':
 
     cnt = 0
@@ -46,12 +60,18 @@ if __name__ == '__main__':
     A = [len(x) for x in param_values]
     configs_num = functools.reduce(lambda x, y: x*y, A)
 
-    for elem in hyper_gen():
+    CONFIGS = [x for x in hyper_gen()]
+    DONE = [x for x in done_gen()]
+
+    for i in range(len(CONFIGS)):
+        elem = CONFIGS[i]
+        done = mult(DONE[i][1:])
         s = ' '.join(elem)
         print('')
         print(s)
+        print(done)
         cmd = 'python train_student.py --gpu %d --part 9 --distill %s --model_s MobileNetV2Trofim -r 1 -a 0 --epochs 100 --nce_k 4096 %s --prefix %s_tune/%s_per11_%d_ ' % (gpu, distill_type, s, distill_type, distill_type, cnt % configs_num)
         print(cmd)
-        if not is_dry_run:
+        if not is_dry_run and not done:
             os.system(cmd)
         cnt += 1
