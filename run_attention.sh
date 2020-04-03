265ec56053501a3a535733a60e89a57fa406d62d
IlyaTrofimov <ilya.trofimov@skoltech.ru>
2020-04-03 12:42:39 +0300

    Add attention tester script.

---
+++ run_attention.sh
@@ -0,0 +1,9 @@
+distill=attention
+gpu=$1
+
+for part in 9 27 3 ; do
+    for i in {0..99} ; do
+        echo $i;
+        echo "python train_student.py --gpu $gpu --part $part --distill $distill --model_s MobileNetV2Trofim -r 1 -a 0 -b 1e3 --epochs 100 --arc $i --prefix $distill/part_$part"
+    done
+done
