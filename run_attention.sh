f9da53c9bcba93787543cc1080414cabc27acc57
Ilya Trofimov <i.trofimov@skoltech.ru>
2020-04-05 09:51:38 +0300

    Update part train scripts.

--- run_attention.sh
+++ run_attention.sh
@@ -3,7 +3,7 @@ gpu=$1
 
 for part in 9 27 3 ; do
     for i in {0..99} ; do
-        echo $i;
-        echo "python train_student.py --gpu $gpu --part $part --distill $distill --model_s MobileNetV2Trofim -r 1 -a 0 -b 1e3 --epochs 100 --arc $i --prefix $distill/part_$part"
+        echo $part $i;
+        python train_student.py --gpu $gpu --part $part --distill $distill --model_s MobileNetV2Trofim -r 1 -a 0 -b 1e3 --epochs 100 --arc $i --prefix $distill/part_$part;
     done
 done
