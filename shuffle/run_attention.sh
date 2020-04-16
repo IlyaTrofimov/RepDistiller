39be9aceaf593ce09a8e19c076c28926d8245b4f
Ilya Trofimov <i.trofimov@skoltech.ru>
2020-04-16 12:19:04 +0300

    Add shell train scripts.

--- shuffle/run_attention.sh
+++ shuffle/run_attention.sh
@@ -4,7 +4,7 @@ gpu=$1
 cd ..
 
 for part in 27 9 3; do
-    for arc in {0..999}; do
-        python train_student.py --distill $distill --kd_T 32 --gpu $gpu --part $part --model_s ShuffleV2 --path_t shufflev2_teacher.pth --prefix shuffle/$distill/part_$part -r 1 -a 0 -b 1e3 --epochs 100 --arc $arc --val_freq 10
+    for arc in {0..300}; do
+        python train_student.py --distill $distill --gpu $gpu --part $part --model_s ShuffleV2 --path_t shufflev2_teacher.pth --prefix shuffle/$distill/part_$part -r 1 -a 0 -b 4e3 --epochs 100 --arc $arc --val_freq 10
     done
 done
---
+++ shuffle/run_similarity.sh
@@ -0,0 +1,10 @@
+distill=similarity
+gpu=$1
+
+cd ..
+
+for part in 27 9 3; do
+    for arc in {0..300}; do
+        python train_student.py --distill $distill --gpu $gpu --part $part --model_s ShuffleV2 --path_t shufflev2_teacher.pth --prefix shuffle/$distill/part_$part -r 1 -a 0 -b 0.09e3 --epochs 100 --arc $arc --val_freq 10
+    done
+done
